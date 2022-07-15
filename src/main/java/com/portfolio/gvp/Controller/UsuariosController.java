350a449154e06608283713299ddc257c429cc480
Gabriel151188 <gvillanus@gmail.com>
2022-07-15 16:34:59 -0300

    Cambio de la url de la WEB

--- src/main/java/com/portfolio/gvp/Controller/UsuariosController.java
+++ src/main/java/com/portfolio/gvp/Controller/UsuariosController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://villanueva-gabriel.web.app")
 public class UsuariosController {
     @Autowired IUsuariosService iusuariosService;
     
